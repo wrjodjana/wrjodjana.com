d7561a5fe17e7585b4f8c77702b024ae22fb3796
wrjodjana <141975732+wrjodjana@users.noreply.github.com>
2024-06-23 02:34:28 -0500

    added links to website

--- app/components/Information/page.tsx
+++ app/components/Information/page.tsx
@@ -18,18 +18,31 @@ export default function InformationSection() {
         <a href="https://linkedin.com/in/wrjodjana" className="text-blue-700 text-2xl">
           <FaLinkedin />
         </a>
-        <a href="/resume.pdf" className="text-green-600 text-2xl">
-          <FaFileAlt />
-        </a>
       </div>
       <p className="mt-4">
         Hi! I&apos;m Warren, a student at the University of Illinois studying Computer Science and Geographic Information Science. I&apos;m mostly interested in full-stack web development, geospatial analysis and data science (AI/ML).
       </p>
       <p className="mt-4">
-        On campus, I&apos;m currently a research assistant at the Uncertainty Quantification Group and a backend engineer intern at Tootak. Previously, I did research in the special education department and was a course assistant for the
-        largest CS class, CS 124.
+        On campus, I&apos;m currently a research assistant at the{" "}
+        <a href="https://uq.cee.illinois.edu/" className="text-blue-600 hover:underline" target="_blank" rel="noopener noreferrer">
+          Uncertainty Quantification Group
+        </a>{" "}
+        and a backend engineer intern at{" "}
+        <a href="https://tootak.org/en/home/" className="text-blue-600 hover:underline" target="_blank" rel="noopener noreferrer">
+          Tootak
+        </a>
+        . Previously, I did research in the special education department and was a course assistant for the largest CS class,{" "}
+        <a href="https://www.cs124.org/" className="text-blue-600 hover:underline" target="_blank" rel="noopener noreferrer">
+          CS 124
+        </a>
+        .
       </p>
-      <p className="mt-4">I&apos;m currently based in Urbana, Illinois but I&apos;ve lived in Jakarta and Surabaya previously. I am also a huge NBA and VCT fan and also like spicy food.</p>
+      <p className="mt-4">
+        I&apos;m currently based in Urbana, Illinois but I&apos;ve lived in Jakarta and Surabaya previously. I am also a huge NBA and VCT fan and also like{" "}
+        <a href="https://en.wikipedia.org/wiki/Ayam_penyet" className="text-blue-600 hover:underline" target="_blank" rel="noopener noreferrer">
+          spicy food.
+        </a>
+      </p>
     </header>
   );
 }
