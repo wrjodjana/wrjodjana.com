561f521f17d0acc16d5184a5bf9b64b8f56a02ee
wrjodjana <warrjodjana@gmail.com>
2025-10-11 17:50:24 -0500

    changed last para

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,13 @@ export default function Home() {
 
             <p className="mb-4 leading-none">i am currently studying stats and cs at uiuc. my interests include software development, mle and ai.</p>
             <p className="mb-4 leading-none">i am originally from jakarta and surabaya, indonesia. last summer i interned at extrahop networks in seattle building an ai agent to generate detector changelogs.</p>
-            <p className="mb-4 leading-none">i also enjoy working at the intersection of technology and environment, specifically within transportation and agriculture.</p>
+            <p className="mb-4 leading-none">
+              i first became interested in technology and startups after reading{" "}
+              <a href="https://en.wikipedia.org/wiki/Delivering_Happiness" style={{ textDecoration: "underline" }}>
+                delivering happiness
+              </a>{" "}
+              by tony hsieh when i was 13.
+            </p>
 
             <h2 className="text-[18px] font-bold mt-0 mb-1 leading-none">contacts</h2>
             <div className="mt-1 mb-4">
